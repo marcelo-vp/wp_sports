7fa872005716ee9d146f5a894774bab7b667ee88
Marcelo Pinto <marcelos.mail@gmail.com>
2017-06-03 15:15:43 -0300

    Registered the calendar widget area, to display a list of events

--- functions.php
+++ functions.php
@@ -322,6 +322,16 @@ function twentyseventeen_widgets_init() {
 	) );
 
 	register_sidebar( array(
+		'name'          => __( 'Calendar', 'twentyseventeen' ),
+		'id'            => 'calendar-1',
+		'description'   => __( 'Add calendar widgets here to appear in your calendar area.', 'twentyseventeen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
 		'name'          => __( 'Footer 1', 'twentyseventeen' ),
 		'id'            => 'sidebar-2',
 		'description'   => __( 'Add widgets here to appear in your footer.', 'twentyseventeen' ),
