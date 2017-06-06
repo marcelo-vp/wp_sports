a9c7b0d07a035ad880dad3c93721bc05fcbe5e54
Marcelo Pinto <marcelos.mail@gmail.com>
2017-06-06 15:19:56 -0300

    Created the 'Contact page' page template

---
+++ page-contato.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Specific template for the contact page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post(); // Start of loop.
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+			<!-- Google Maps API -->
+			<article class="google-map">
+				<strong>Mapa</strong>
+				<div id="googleMapS"></div>
+			</article>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
