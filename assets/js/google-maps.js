ee602ccaccf603ae4e495ed395532ad6fd454783
Marcelo Pinto <marcelos.mail@gmail.com>
2017-06-06 18:08:25 -0300

    Created Google Maps API javascript file

---
+++ assets/js/google-maps.js
@@ -0,0 +1,26 @@
+/*Google Maps | Development API Key: AIzaSyDnzQpNe1LnsQyYZSpvSunIgLQUagtHhdw */
+    
+function initMap() {
+
+	/* Sets marker location */
+	var location = new google.maps.LatLng( , );
+
+	/* Sets map config */
+	var mapSConfig = {
+		center:location,
+		zoom:15,
+		mapTypeId:google.maps.MapTypeId.ROADMAP,
+		draggable:false,
+		scrollwheel:false
+	};
+
+	/* Create map based on config and insert content in the DOM using the element's ID */
+	var mapSmall = new google.maps.Map(document.getElementById("googleMapS"),mapSConfig);
+
+	/* Create a marker and choose a map to place it in */
+	var markerS = new google.maps.Marker({
+		position:location,
+		map:mapSmall
+	});
+		
+}
