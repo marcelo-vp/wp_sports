25ac390ea6f368932f90dadeebf56ef76b18400a
Marcelo Pinto <marcelos.mail@gmail.com>
2017-06-08 10:28:27 -0300

    Customized map colors using Google Maps API

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,4 +1,4 @@
-/*Google Maps | Development API Key: AIzaSyDnzQpNe1LnsQyYZSpvSunIgLQUagtHhdw */
+/* Google Maps | Development API Key: AIzaSyDnzQpNe1LnsQyYZSpvSunIgLQUagtHhdw */
     
 function initMap() {
 
@@ -10,7 +10,57 @@ function initMap() {
 		center:location,
 		zoom:15,
 		mapTypeId:google.maps.MapTypeId.ROADMAP,
-		scrollwheel:false
+		scrollwheel:false,
+		styles: [
+			{
+				'featureType' : 'landscape',
+				'elementType' : 'geometry',
+				'stylers' : [
+					{ 'color' : '#FFE0B2' } /* orange-100 */
+				]
+			},
+			{
+				'featureType' : 'poi',
+				'elementType' : 'geometry',
+				'stylers' : [
+					{ 'color' : '#FFCC80' } /* orange-200 */
+				]
+			},
+			{
+				'featureType' : 'poi',
+				'elementType' : 'labels',
+				'stylers' : [
+					{ 'visibility' : 'off' }
+				]
+			},
+			{
+				'featureType' : 'transit',
+				'elementType' : 'geometry',
+				'stylers' : [
+					{ 'color' : '#FFCC80' } /* orange-200 */
+				]
+			},
+			{
+				'featureType' : 'road',
+				'elementType' : 'geometry',
+				'stylers' : [
+					{ 'color' : '#FFA726' } /* orange-400 */
+				]
+			},
+			{
+				'featureType' : 'water',
+				'elementType' : 'geometry',
+				'stylers' : [
+					{ 'color' : '#EF6C00' } /* orange-800 */
+				]
+			},
+			{
+				'elementType' : 'labels.text.fill',
+				'stylers' : [
+					{ 'color' : '#333333' }
+				]
+			}
+		]
 	};
 
 	/* Create map based on config and insert content in the DOM using the element's ID */
